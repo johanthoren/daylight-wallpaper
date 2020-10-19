915a9498babd2c9aaa0270d46999d9820c4b70d8
Johan Thoren <jthoren@itrsgroup.com>
2020-10-19 11:20:21 +0200

    Organize by function.

--- daylight_wallpaper.sh
+++ daylight_wallpaper.sh
@@ -22,9 +22,9 @@
 
 usage() {
     cat <<EOF
-Usage: $0 -h | [-d] -x LATITUDE -y LONGITUDE -f FOLDER"
+Usage: $0 -h | [-d] -x LATITUDE -y LONGITUDE -f FOLDER
 
-       The FOLDER needs to contain the following images:"
+       The FOLDER needs to contain the following images:
        - night.jpg"
        - nautical_dawn.jpg
        - civil_dawn.jpg
@@ -39,45 +39,38 @@ EOF
 
 debug=0
 
-while getopts "dhx:y:f:" opt
-   do
-     case $opt in
-        x) LAT=$OPTARG;;
-        y) LONG=$OPTARG;;
-        f) folder=$OPTARG;;
-        d) debug=1;;
-        h) usage 0;;
-        *) usage 1;;
-
-     esac
-done
-
-# Make sure that all required commands are available.
-if ! command -v curl > /dev/null 2>&1; then
-    echo "ERROR: curl is not installed"
-    exit 1
-elif ! command -v feh > /dev/null 2>&1; then
-    echo "ERROR: feh is not installed"
-    exit 1
-elif ! command -v jq > /dev/null 2>&1; then
-    echo "ERROR: jq is not installed"
-    exit 1
-fi
-
-# Make sure that all required variables are set.
-[ -z "$LAT" ] || [ -z "$LONG" ] || [ -z "$folder" ] && usage 1
-
-# Set the boundraries of the current day.
-DAY_BEGIN="$(date --date "$(date --iso)" +"%s")"
-DAY_END="$(date --date "$(date --iso) 23:59:59" +"%s")"
-
 print_debug() {
     [ "$debug" -eq 1 ] && echo "$1"
 }
 
+verify_requirements() {
+    # Make sure that all required commands are available.
+    if ! command -v curl > /dev/null 2>&1; then
+        echo "ERROR: curl is not installed"
+        exit 1
+    elif ! command -v feh > /dev/null 2>&1; then
+        echo "ERROR: feh is not installed"
+        exit 1
+    elif ! command -v jq > /dev/null 2>&1; then
+        echo "ERROR: jq is not installed"
+        exit 1
+    fi
+
+    # Make sure that all required variables are set.
+    [ -z "$LAT" ] || [ -z "$LONG" ] || [ -z "$folder" ] && usage 1
+}
+
+# Set the boundraries of the current day.
+define_day() {
+    DAY_BEGIN="$(date --date "$(date --iso)" +"%s")"
+    print_debug "The day begins at $DAY_BEGIN"
+    DAY_END="$(date --date "$(date --iso) 23:59:59" +"%s")"
+    print_debug "The day ends at $DAY_END"
+}
+
 # Fetch the Sunrise and Sunset data from https://sunrise-sunset.org/api
 fetch_sun_data() {
-    print_debug "Performing new sun_data fetch."
+    print_debug "Performing new sun_data fetch"
     print_debug "Deleting old sun_data files"
     find /tmp -maxdepth 1 -name "sun_data*.json" -user "$USER" -delete
 
@@ -120,8 +113,6 @@ check_local_sun_data() {
     fi
 }
 
-check_local_sun_data
-
 # Parse the json response to extract the wanted string.
 parse_response() {
     if [ "$#" -eq 1 ]; then
@@ -137,34 +128,50 @@ parse_response() {
     fi
 }
 
-# Exit if the fetch was not "OK".
-API_STATUS="$(parse_response status)"
-print_debug "API Status: $API_STATUS"
+# Exit if the sun_data is not "OK".
+verify_sun_data() {
+    API_STATUS="$(parse_response status)"
+    print_debug "API Status: $API_STATUS"
 
-if [ "$API_STATUS" != "OK" ]; then
-    echo "ERROR: The API request did not finish with an \"OK\" status"
-    exit 1
-fi
+    if [ "$API_STATUS" != "OK" ]; then
+        echo "ERROR: The API request did not finish with an \"OK\" status"
+        exit 1
+    fi
+}
 
 # Populate the vars to compare against. In chronological order.
-NAUT_TWI_BEGIN="$(parse_response results nautical_twilight_begin)"
-CIV_TWI_BEGIN="$(parse_response results civil_twilight_begin)"
-SUNRISE="$(parse_response results sunrise)"
-NOON="$(parse_response results solar_noon)"
-SUNSET="$(parse_response results sunset)"
-# I want to switch to another wallpaper when the afternoon is starting to head
-# toward sunset.
-LENGTH_OF_AFTERNOON=$((SUNSET-NOON))
-LATE_AFTERNOON=$((NOON+LENGTH_OF_AFTERNOON/2))
-CIV_TWI_END="$(parse_response results civil_twilight_end)"
-NAUT_TWI_END="$(parse_response results nautical_twilight_end)"
+populate_vars(){
+    NAUT_TWI_BEGIN="$(parse_response results nautical_twilight_begin)"
+    CIV_TWI_BEGIN="$(parse_response results civil_twilight_begin)"
+    SUNRISE="$(parse_response results sunrise)"
+    NOON="$(parse_response results solar_noon)"
+    SUNSET="$(parse_response results sunset)"
+    # I want to switch to another wallpaper when the afternoon is starting to head
+    # toward sunset.
+    LENGTH_OF_AFTERNOON=$((SUNSET-NOON))
+    LATE_AFTERNOON=$((NOON+LENGTH_OF_AFTERNOON/2))
+    CIV_TWI_END="$(parse_response results civil_twilight_end)"
+    NAUT_TWI_END="$(parse_response results nautical_twilight_end)"
 
-# The local time as a unix timestamp.
-TIME="$(date +"%s")"
+    # The local time as a unix timestamp.
+    TIME="$(date +"%s")"
+}
 
-if [ $debug -eq 1 ]; then
-    cat <<EOF
-The day begins at $DAY_BEGIN
+determine_period() {
+    [ "$TIME" -ge "$NAUT_TWI_END" ] || [ "$TIME" -lt "$NAUT_TWI_BEGIN" ] && period="night"
+    [ "$TIME" -ge "$NAUT_TWI_BEGIN" ] && [ "$TIME" -lt "$CIV_TWI_BEGIN" ] && period="nautical_dawn"
+    [ "$TIME" -ge "$CIV_TWI_BEGIN" ] && [ "$TIME" -lt "$SUNRISE" ] && period="civil_dawn"
+    [ "$TIME" -ge "$SUNRISE" ] && [ "$TIME" -lt "$NOON" ] && period="morning"
+    [ "$TIME" -ge "$NOON" ] && [ "$TIME" -lt "$LATE_AFTERNOON" ] && period="noon"
+    [ "$TIME" -ge "$LATE_AFTERNOON" ] && [ "$TIME" -lt "$SUNSET" ] && period="late_afternoon"
+    [ "$TIME" -ge "$SUNSET" ] && [ "$TIME" -lt "$CIV_TWI_END" ] && period="civil_dusk"
+    [ "$TIME" -ge "$CIV_TWI_END" ] && [ "$TIME" -lt "$NAUT_TWI_END" ] && period="nautical_dusk"
+    [ -z "$period" ] && echo "ERROR: Unable to determine period" && exit 1
+}
+
+debug_summary() {
+    if [ $debug -eq 1 ]; then
+        cat <<EOF
 The nautical twilight begins at $NAUT_TWI_BEGIN
 The civil twilight begins at $CIV_TWI_BEGIN
 The sunrise is at $SUNRISE
@@ -173,26 +180,44 @@ The late afternoon begins at $LATE_AFTERNOON
 The sunset is at $SUNSET
 The civil twilight ends at $CIV_TWI_END
 The nautical twilight ends at $NAUT_TWI_END
-The day ends at $DAY_END
 The time is now $TIME
 EOF
-fi
+    fi
+}
 
-[ "$TIME" -ge "$NAUT_TWI_END" ] || [ "$TIME" -lt "$NAUT_TWI_BEGIN" ] && period="night"
-[ "$TIME" -ge "$NAUT_TWI_BEGIN" ] && [ "$TIME" -lt "$CIV_TWI_BEGIN" ] && period="nautical_dawn"
-[ "$TIME" -ge "$CIV_TWI_BEGIN" ] && [ "$TIME" -lt "$SUNRISE" ] && period="civil_dawn"
-[ "$TIME" -ge "$SUNRISE" ] && [ "$TIME" -lt "$NOON" ] && period="morning"
-[ "$TIME" -ge "$NOON" ] && [ "$TIME" -lt "$LATE_AFTERNOON" ] && period="noon"
-[ "$TIME" -ge "$LATE_AFTERNOON" ] && [ "$TIME" -lt "$SUNSET" ] && period="late_afternoon"
-[ "$TIME" -ge "$SUNSET" ] && [ "$TIME" -lt "$CIV_TWI_END" ] && period="civil_dusk"
-[ "$TIME" -ge "$CIV_TWI_END" ] && [ "$TIME" -lt "$NAUT_TWI_END" ] && period="nautical_dusk"
-[ -z "$period" ] && echo "ERROR: Unable to determine period" && exit 1
+set_wallpaper() {
+    # Remove any trailing slash before trying to use the folder.
+    trimmed_folder="${folder%/}"
+    WALLPAPER="${trimmed_folder}/${period}.jpg"
 
-# Remove any trailing slash before trying to use the folder.
-trimmed_folder="${folder%/}"
-WALLPAPER="${trimmed_folder}/${period}.jpg"
+    print_debug "It's currently: $period"
+    print_debug "Setting the wallpaper: $WALLPAPER"
 
-print_debug "It's currently: $period"
-print_debug "Setting the wallpaper: $WALLPAPER"
+    feh --bg-fill "$WALLPAPER"
+}
 
-feh --bg-fill "$WALLPAPER"
+while getopts "dhx:y:f:" opt
+   do
+     case $opt in
+        x) LAT=$OPTARG;;
+        y) LONG=$OPTARG;;
+        f) folder=$OPTARG;;
+        d) debug=1;;
+        h) usage 0;;
+        *) usage 1;;
+
+     esac
+done
+
+main() {
+    verify_requirements
+    define_day
+    check_local_sun_data
+    verify_sun_data
+    populate_vars
+    determine_period
+    debug_summary
+    set_wallpaper
+}
+
+main
